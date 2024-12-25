5641ce16ec4fae495e5fad1b71fc496044b9ff12
Beauti <183709979+beautisui@users.noreply.github.com>
2024-12-25 11:42:27 +0530

    update_program

--- data_manage.js
+++ data_manage.js
@@ -1,17 +1,17 @@
 const Rahul = {
   name: "Rahul",
-  age: null,
+  age: 30,
   city: "Pune",
   occupation: "software engineer",
   hobbies: ["gardening"],
-  pet: [
+  pets: [
     {
       breed: "golden retriver",
       name: "Max",
       age: 4,
       vaccination: true,
       fullyVaccinated: true,
-      likes: ["loves playing in the park"],
+      favActivities: ["loves playing in the park"],
     },
   ],
   employed: true,
@@ -24,14 +24,14 @@ const Ananya = {
   city: "Bangalore",
   age: 30,
   hobbies: ["cooking", "often experiments with Italian recipes"],
-  pet: [
+  pets: [
     {
       breed: "bird",
       name: "Kiwi",
       age: 20,
       vaccination: true,
       fullyVaccinated: false,
-      likes: ["mimics Ananya's voice"],
+      favActivities: ["mimics Ananya's voice"],
     },
   ],
   employed: false,
@@ -49,14 +49,14 @@ const Ramesh = {
     "reading historical fiction",
     "tending to his rose garden",
   ],
-  pet: [
+  pets: [
     {
       breed: "cat",
       name: "Bella",
       vaccination: true,
       fullyVaccinated: true,
       age: 3,
-      likes: ["love lounging in the sun"],
+      favActivities: ["love lounging in the sun"],
     },
     {
       breed: "cat",
@@ -64,7 +64,7 @@ const Ramesh = {
       vaccination: true,
       fullyVaccinated: true,
       age: 3,
-      likes: ["love lounging in the sun"],
+      favActivities: ["love lounging in the sun"],
     },
   ],
   employed: true,
@@ -81,14 +81,17 @@ const Kavya = {
     "prefers modern fantasy novels",
     "binge-watching sci-fi shows in downtime",
   ],
-  pet: [
+  pets: [
     {
       breed: "rabbit",
       name: "Snowy",
       age: 2,
       vaccination: true,
       fullyVaccinated: false,
-      likes: ["enjoys hopping around her backyard", "nibbling on carrots"],
+      favActivities: [
+        "enjoys hopping around her backyard",
+        "nibbling on carrots",
+      ],
     },
   ],
   employed: false,
@@ -108,23 +111,24 @@ const peopleHaveCar = (records) => {
   return records.filter(({ ownsCar }) => ownsCar).length;
 };
 
-const fullVaccinatedPetsCount = (totalVaccinated, record) => {
-  const { pet: petDetails } = record;
-  totalVaccinated += petDetails.filter(
+const fullVaccinatedpetsCount = (totalVaccinated, record) => {
+  const { pets: petsDetails } = record;
+  totalVaccinated += petsDetails.filter(
     ({ fullyVaccinated }) => fullyVaccinated
   ).length;
+
   return totalVaccinated;
 };
 
 const vaccinatedPets = (records) => {
-  return records.reduce(fullVaccinatedPetsCount, 0);
+  return records.reduce(fullVaccinatedpetsCount, 0);
 };
 
 const nameAndType = (record) => {
-  return record.pet.map(({ name, breed }) => [name, breed]);
+  return record.pets.map(({ name, breed }) => [name, breed]);
 };
 
-const petNameWithType = (records) => {
+const petsNameWithType = (records) => {
   return records.flatMap(nameAndType);
 };
 
@@ -135,11 +139,12 @@ const cities = (records) => {
 const totalHobbies = (records) => {
   const hobbie = [...new Set(records.flatMap(({ hobbies }) => hobbies))];
 
-  return { tatalHobbies: hobbie.length, hobbies: hobbie };
+  return { totalHobbies: hobbie.length, hobbie };
 };
 
 const averageOf = (numberList) => {
   const sumOfNumbers = numberList.reduce((sum, number) => sum + number, 0);
+
   return sumOfNumbers / numberList.length;
 };
 
@@ -147,27 +152,28 @@ const averageAge = (records) => {
   return averageOf(records.map(({ age }) => age));
 };
 
-const studiedCsAndHavePets = (records) => {
+const studiedCsAndHavepets = (records) => {
   const csStudentsDetail = records.filter(
     ({ education }) => education === "computer science"
   );
 
-  const csStudentWithPets = csStudentsDetail.filter(
-    ({ pet }) => pet.length !== 0
+  const csStudentWithpets = csStudentsDetail.filter(
+    ({ pets }) => pets.length !== 0
   );
 
-  return csStudentWithPets.length;
+  return csStudentWithpets.length;
 };
 
 const personWithMoreThan2pets = (records) => {
-  return records.filter(({ pet }) => pet.length > 1).length;
+  return records.filter(({ pets }) => pets.length > 1).length;
 };
 
-const petsHaveLikes = function (records) {
-  const petList = records.flatMap(({ pet }) => pet);
-  return petList
-    .filter(({ likes }) => likes.length !== 0)
-    .map(({ name, likes }) => [name, likes].flat());
+const petsAndFavActivities = function (records) {
+  const petsList = records.flatMap(({ pets }) => pets);
+
+  return petsList
+    .filter(({ favActivities }) => favActivities.length !== 0)
+    .flatMap(({ name, favActivities }) => [name, favActivities]);
 };
 
 const petsOfBengloreAndChennai = function (records) {
@@ -175,15 +181,17 @@ const petsOfBengloreAndChennai = function (records) {
   const citizenOfBengloreChennai = records.filter(
     ({ city }) => city.includes(place[0]) || city.includes(place[1])
   );
-  return citizenOfBengloreChennai.flatMap(({ pet }) =>
-    pet.map(({ name }) => name)
+
+  return citizenOfBengloreChennai.flatMap(({ pets }) =>
+    pets.map(({ name }) => name)
   );
 };
 
 const vaccinatedPetsOfCarLessOwner = (records) => {
   const carLessPeople = records.filter(({ ownsCar }) => !ownsCar);
-  const petDetails = carLessPeople.flatMap(({ pet }) => pet);
-  return petDetails.filter(({ vaccination }) => vaccination).length;
+  const petsDetails = carLessPeople.flatMap(({ pets }) => pets);
+
+  return petsDetails.filter(({ vaccination }) => vaccination).length;
 };
 
 const moreThan2Hobbies = (records) => {
@@ -191,14 +199,15 @@ const moreThan2Hobbies = (records) => {
 };
 
 const commonPetType = (records) => {
-  const petArray = records.flatMap(({ pet }) => pet);
-  const petNames = petArray.flatMap(({ breed }) => breed);
-  const uniquePets = [...new Set(petNames)];
+  const petsArray = records.flatMap(({ pets }) => pets);
+  const petsNames = petsArray.flatMap(({ breed }) => breed);
+  const uniquepets = [...new Set(petsNames)];
 
-  const occurancesDetails = uniquePets.map((pet) => {
-    const occurances = petNames.filter((x) => x === pet).length;
-    return { times: occurances, types: pet };
+  const occurancesDetails = uniquepets.map((pets) => {
+    const occurances = petsNames.filter((x) => x === pets).length;
+    return { times: occurances, types: pets };
   });
+
   const occurrenceList = occurancesDetails.map(({ times }) => times);
   const heighestOccurance = Math.max(...occurrenceList);
   return occurancesDetails.find(({ times }) => times === heighestOccurance)
@@ -206,32 +215,37 @@ const commonPetType = (records) => {
 };
 
 const youngestPet = (records) => {
-  const petsNameAges = records.flatMap(({ pet }) =>
-    pet.map(({ name, age }) => ({ name, age }))
+  const petsNameAges = records.flatMap(({ pets }) =>
+    pets.map(({ name, age }) => ({ name, age }))
   );
   const ageList = petsNameAges.map(({ age }) => age);
   const lowestAge = Math.min(...ageList);
+
   return petsNameAges.find(({ age }) => age === lowestAge).name;
 };
 
 const unEmployedPeoplePets = (record) => {
   const unemployedPeopleDetails = record.filter(({ employed }) => !employed);
-  return unemployedPeopleDetails.map(({ pet }) => pet).length;
+
+  return unemployedPeopleDetails.map(({ pets }) => pets).length;
 };
 
-const peopleWhoLiveInCityStartsWithB = (records) => {
+const peopleLiveInCityStartsWithB = (records) => {
   return records.filter(({ city }) => city.at(0) === "B").length;
 };
 
-const petLessPeople = (record) => {
-  const petlessPeopleDetails = record.filter(({ pet }) => pet.length === 0);
-  return petlessPeopleDetails.map(({ name }) => ({ name }));
+const peopleWithoutPets = (record) => {
+  const petslessPeopleDetails = record.filter(({ pets }) => pets.length === 0);
+
+  return petslessPeopleDetails.map(({ name }) => ({ name }));
 };
 
 const getQuestionAns = (Qn, fn) => {
   console.log("Question:", Qn);
   console.log("Answer  :", fn(details));
 };
+
+//-----------------------------------------------------------------------------
 
 function testAll() {
   getQuestionAns(
@@ -242,7 +256,7 @@ function testAll() {
   getQuestionAns("3. How many pets are fully vaccinated?", vaccinatedPets);
   getQuestionAns(
     "4. What are the names of all the pets, and what type of animal is each?",
-    petNameWithType
+    petsNameWithType
   );
   getQuestionAns("5. Which cities do the individuals live in?", cities);
   getQuestionAns(
@@ -259,15 +273,15 @@ function testAll() {
   );
   getQuestionAns(
     "9. How many individuals have studied computer science, and how many of them have pets?",
-    studiedCsAndHavePets
+    studiedCsAndHavepets
   );
   getQuestionAns(
-    "10. How many individuals own more than one pet?",
+    "10. How many individuals own more than one pets?",
     personWithMoreThan2pets
   );
   getQuestionAns(
     "11. Which pets are associated with specific favorite activities?",
-    petsHaveLikes
+    petsAndFavActivities
   );
   getQuestionAns(
     "12. What are the names of all animals that belong to people who live in Bangalore or Chennai?.",
@@ -275,10 +289,10 @@ function testAll() {
   );
   getQuestionAns(
     "13. How many vaccinated pets belong to people who do not own a car?",
-    vaccinatedPetsOfCarLessOwner
+    vaccinatedPetsOfCarLessOwner //
   );
   getQuestionAns(
-    "14. What is the most common type of pet among the group?.",
+    "14. What is the most common type of pets among the group?.",
     commonPetType
   );
   getQuestionAns(
@@ -286,14 +300,17 @@ function testAll() {
     moreThan2Hobbies
   );
   getQuestionAns(
-    "17. Which pet is the youngest, and what is its name?",
+    "17. Which pets is the youngest, and what is its name?",
     youngestPet
   );
   getQuestionAns(
     "19. How many individuals live in cities starting with the letter B?",
-    peopleWhoLiveInCityStartsWithB
+    peopleLiveInCityStartsWithB
   );
-  getQuestionAns("20. Which individuals do not own any pets?", petLessPeople);
+  getQuestionAns(
+    "20. Which individuals do not own any pets?",
+    peopleWithoutPets
+  );
 }
 
 testAll();
